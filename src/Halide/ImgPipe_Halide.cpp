5955911b0000a950b829bfdf37332e7c18f46c64
mbuckler <markabuckler@gmail.com>
2016-07-06 20:37:28 -0400

    Functional color transform

--- src/Halide/ImgPipe_Halide.cpp
+++ src/Halide/ImgPipe_Halide.cpp
@@ -7,11 +7,12 @@
 #include "LoadCamModel.h"
 #include "MatrixOps.h"
 #include <stdio.h>
+#include <math.h>
 #include "halide_image_io.h"
 
 // Function prototypes
-int run_pipeline(bool direction);
-
+int   run_pipeline(bool direction);
+float degreen(float in);
 
 int main(int argc, char **argv) {
   using namespace std;
@@ -53,46 +54,79 @@ int run_pipeline(bool direction) {
     throw runtime_error("Error: TsTw != Ts*Tw. Possible model import error or improper model format");
 */
  
-
+  // Take the transpose of the color map and white balance transform for later use
+  vector<vector<float>> TsTw_tran = transpose_mat (TsTw);
 
 
   using namespace Halide;
-
   using namespace Halide::Tools;
 
-
+  // Load input image 
   Image<uint8_t> input = load_image(demosaiced_image);
 
-  // Next we define our Func object that represents our stage
-  Halide::Func brighter;
-  Halide::Var x, y, c;
+  // Declare image handle variables
+  Var x, y, c;
 
-  // For each pixel of the input image.
-  Halide::Expr value = input(x, y, c);
+  Func input_16("input_16");
+  input_16(x,y,c)  = cast<uint16_t>(input(x,y,c));
 
-  // Selectively brighten
-  value = select(c == 0, input(x,y,c) * 2.0f,
-                 c == 1, input(x,y,1)*1.0f+input(x,y,2)*1.0f ,
-                       input(x,y,c)*1.0f);
 
-  // Clamp it to be less than 255
-  value = Halide::min(value, 255.0f);
+  // Color map and white balance transform
+  Func transform("transform");
+  transform(x,y,c) = max( select(
+    // Perform matrix multiplication, set min of 0
+    c == 0, input_16(x,y,0)*TsTw_tran[0][0] + input_16(x,y,1)*TsTw_tran[1][0] + input_16(x,y,2)*TsTw_tran[2][0],
+    c == 1, input_16(x,y,0)*TsTw_tran[0][1] + input_16(x,y,1)*TsTw_tran[1][1] + input_16(x,y,2)*TsTw_tran[2][1],
+            input_16(x,y,0)*TsTw_tran[0][2] + input_16(x,y,1)*TsTw_tran[1][2] + input_16(x,y,2)*TsTw_tran[2][2])
+                          , 0);
+ 
+/*
+  // Weighted radial basis function for gamut mapping
+  Func rbf_ctrl_pts("rbf_ctrl_pts");
+  // Initialization:     red_val, green_val, blue_val
+  rbf_ctrl_pts(x,y,c) = {      0,         0,        0};
+  // Index to iterate with
+  RDom idx(0,num_ctrl_pts-1);
+  // Loop code
+  // Subtract the vectors 
+  Expr red_sub   = transform(x,y,0) - ctrl_pts[r][0];
+  Expr green_sub = transform(x,y,1) - ctrl_pts[r][1];
+  Expr blue_sub  = transform(x,y,2) - ctrl_pts[r][2];
+  // Take the L2 norm to get the distance
+  Expr dist      = sqrtf( powf(red_sub,2)+powf(green_sub,2)+powf(blue_sub,2) );
+  // Extract the old values
+  Expr old_red   = rbf_ctrl_pts(x,y,c)
 
-  // Cast it back to an 8-bit unsigned integer.
-  value = Halide::cast<uint8_t>(value);
+  // Update persistant loop variables
+  rbf_ctrl_pts(x,y,c) = { , , };
+*/
 
-  // Define the function.
-  brighter(x, y, c) = value;
-
-  // The equivalent one-liner to all of the above is:
-  // brighter(x, y, c) = Halide::cast<uint8_t>(min(input(x, y, c) * 1.5f, 255));
+  Func output_8("output_8");
+  output_8(x,y,c) = cast<uint8_t>(transform(x,y,c));
 
   // Realize the function
-  Halide::Image<uint8_t> output =
-      brighter.realize(input.width(), input.height(), input.channels());
+  Image<uint8_t> output =
+      output_8.realize(input.width(), input.height(), input.channels());
+
+
+  for (int i=0;i<input.width();i++) {
+    for (int j=0;j<input.height();j++) {
+      for (int rgb=0;rgb<3;rgb++) {
+        output(i,j,rgb) = output(i,j,rgb) * 0.5f; 
+      }
+    }
+  }
+
 
   // Save the output for inspection. It should look like a bright parrot.
-  save_image(output, "brighter.png");
+  save_image(output, "output.png");
 
   return 0;
 }
+
+float degreen(float in) {
+  
+  return in * 0.5;
+
+}
+
