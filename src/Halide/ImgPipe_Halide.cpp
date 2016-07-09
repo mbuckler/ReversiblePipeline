b3a7872ddfe18e6df3c82362dc5fef91602067b9
mbuckler <markabuckler@gmail.com>
2016-07-08 21:41:04 -0400

    Fully functional forward pipeline!

--- src/Halide/ImgPipe_Halide.cpp
+++ src/Halide/ImgPipe_Halide.cpp
@@ -11,23 +11,29 @@
 #include "halide_image_io.h"
 
 // Function prototypes
-int   run_pipeline(bool direction);
-float degreen(float in);
+int   run_pipeline(bool direction, int patchsize, int xstart, int ystart);
+
 
 int main(int argc, char **argv) {
   using namespace std;
 
+  int patchsize = 1;
+  int xstart    = 551;
+  int ystart    = 2751; 
+
   // Run forward pipeline
-  run_pipeline(0);
+  run_pipeline(0, patchsize, xstart, ystart);
 
   // Run backward pipeline
-  //run_pipeline(1);
+  //run_pipeline(1, patchsize, xstart, ystart);
 
   printf("Success!\n");
   return 0;
 }
 
-int run_pipeline(bool direction) {
+
+// Reversible pipeline function
+int run_pipeline(bool direction, int patchsize, int xstart, int ystart) {
 
   using namespace std;  
 
@@ -61,16 +67,16 @@ int run_pipeline(bool direction) {
   using namespace Halide;
   using namespace Halide::Tools;
 
-  // Convert control point and weights to Halide Images
+  // Convert control points to a Halide image
   int width  = ctrl_pts[0].size();
   int length = ctrl_pts.size();
   Image<float> ctrl_pts_h(width,length);
   for (int y=0; y<length; y++) {
     for (int x=0; x<width; x++) {
-      // Scale the control points to fit 8 bits
-      ctrl_pts_h(x,y) = 256 * ctrl_pts[y][x];
+      ctrl_pts_h(x,y) = ctrl_pts[y][x];
     }
   }
+  // Convert weights to a Halide image
   width  = weights[0].size();
   length = weights.size();
   Image<float> weights_h(width,length);
@@ -79,55 +85,44 @@ int run_pipeline(bool direction) {
       weights_h(x,y) = weights[y][x];
     }
   }
-
-  // Scale the bias coeficients to fit 8 bits
-  //for (int i=0;i<4;i++) {
-    for (int j=0;j<3;j++) {
-      coefs[0][j] = 256 * coefs[0][j];
+  // Convert the reverse tone mapping function to a Halide image
+  width  = 3;
+  length = 256;
+  Image<float> rev_tone_h(width,length);
+  for (int y=0; y<length; y++) {
+    for (int x=0; x<width; x++) {
+      rev_tone_h(x,y) = rev_tone[y][x];
     }
-  //}
-
-
-
-  int patchsize = 1;
-  int xstart    = 551;
-  int ystart    = 2751;
+  }
  
+
   // Load input image 
   Image<uint8_t> input = load_image(demosaiced_image);
-//  input.set_min(xstart, ystart);
-
- // Define the patch to process
- //  Image<uint8_t> patch(patchsize, patchsize, 3);
- // patch.set_min(xstart, ystart, 0);
-  // Copy the subsection into the patch
-  
-  
-
-
-
   // Declare image handle variables
   Var x, y, c;
 
-  Func input_16("input_16");
-  input_16(x,y,c)  = cast<float>(input(x,y,c));
+
+  // Cast input to float and scale according to its 8 bit input format
+  Func input_scaled("input_scaled");
+  input_scaled(x,y,c)  = cast<float>(input(x,y,c))/256;
   
 
   // Color map and white balance transform
   Func transform("transform");
   transform(x,y,c) = max( select(
     // Perform matrix multiplication, set min of 0
-    c == 0, input_16(x,y,0)*TsTw_tran[0][0]
-          + input_16(x,y,1)*TsTw_tran[1][0]
-          + input_16(x,y,2)*TsTw_tran[2][0],
-    c == 1, input_16(x,y,0)*TsTw_tran[0][1]
-          + input_16(x,y,1)*TsTw_tran[1][1]
-          + input_16(x,y,2)*TsTw_tran[2][1],
-            input_16(x,y,0)*TsTw_tran[0][2]
-          + input_16(x,y,1)*TsTw_tran[1][2]
-          + input_16(x,y,2)*TsTw_tran[2][2])
+    c == 0, input_scaled(x,y,0)*TsTw_tran[0][0]
+          + input_scaled(x,y,1)*TsTw_tran[1][0]
+          + input_scaled(x,y,2)*TsTw_tran[2][0],
+    c == 1, input_scaled(x,y,0)*TsTw_tran[0][1]
+          + input_scaled(x,y,1)*TsTw_tran[1][1]
+          + input_scaled(x,y,2)*TsTw_tran[2][1],
+            input_scaled(x,y,0)*TsTw_tran[0][2]
+          + input_scaled(x,y,1)*TsTw_tran[1][2]
+          + input_scaled(x,y,2)*TsTw_tran[2][2])
                           , 0);
-  transform.trace_stores();
+  //transform.trace_stores();
+
 
   // Weighted radial basis function for gamut mapping
   Func rbf_ctrl_pts("rbf_ctrl_pts");
@@ -149,11 +144,6 @@ int run_pipeline(bool direction) {
   //rbf_ctrl_pts.trace_stores();
 
 
-  // Works!
-  //Image<float> ctrl_pt_out = 
-  //  rbf_ctrl_pts.realize(input.width(), input.height(), input.channels());
-
-
   // Add on the biases for the RBF
   Func rbf_biases("rbf_biases");
   rbf_biases(x,y,c) = max( select( 
@@ -164,15 +154,24 @@ int run_pipeline(bool direction) {
             rbf_ctrl_pts(x,y,2) + coefs[0][2] + coefs[1][2]*transform(x,y,0) +
       coefs[2][2]*transform(x,y,1) + coefs[3][2]*transform(x,y,2))
                           , 0);
-  rbf_biases.trace_stores();
+  //rbf_biases.trace_stores();
 
-  // Cast the output to 8 bit
+
+  // Forward tone mapping
+  Func tonemap("tonemap");
+  RDom idx2(0,256);
+  // Theres a lot in this one line! Functionality wise it finds the entry in 
+  // the reverse tone mapping function which is closest to the value found by
+  // gamut mapping. This entry is then scaled by 256 to ensure that the output
+  // is fractional, just like the rest of the pipeline.
+  tonemap(x,y,c) = argmin( abs( rev_tone_h(c,idx2) - rbf_biases(x,y,c) ) )[0]  / 256.0f;
+  //tonemap.trace_stores();
+
+
+  // Rescale the output and cast the output to 8 bit for image writing
   Func output_8("output_8");
-  output_8(x,y,c) = cast<uint8_t>(rbf_biases(x,y,c));
+  output_8(x,y,c) = cast<uint8_t>(tonemap(x,y,c)*256);
   output_8.trace_stores();
-
-
-
 
 
   // Realize the functions
@@ -180,58 +179,9 @@ int run_pipeline(bool direction) {
   output.set_min(xstart,ystart);
   output_8.realize(output);
 
+
   // Save the output for inspection
   save_image(output, "output.png");
-
-
-
-/*
-  // Further processing
-  //float rbf_vals[3] = {0};
-  //float red_sub, green_sub, blue_sub;
-  //float dist;
-  Func zeros;
-  zeros(x) = 0;
-  Image<float> rbf_vals  = zeros.realize(3);
-  Image<float> red_sub   = zeros.realize(1);
-  Image<float> green_sub = zeros.realize(1);
-  Image<float> blue_sub  = zeros.realize(1);
-  Image<float> dist      = zeros.realize(1);
-
-
-  // Gammut mapping and tone mapping
-  for (int i=0;i<input.width();i++) {
-    for (int j=0;j<input.height();j++) {
-      /////////////////////////////////////////////////////
-      // RBF for gamut mapping
-      // RBF weighted control points
-      for (int pnt_idx=0;pnt_idx<num_ctrl_pts;pnt_idx++) {
-        // Compute distance
-        red_sub(0)   = transformed(x,y,0) - ctrl_pts[pnt_idx][0];
-        green_sub(0) = transformed(x,y,1) - ctrl_pts[pnt_idx][1];
-        blue_sub(0)  = transformed(x,y,2) - ctrl_pts[pnt_idx][2];
-        dist(0)      = sqrtf( powf(red_sub(0),2)+powf(green_sub(0),2)+powf(blue_sub(0),2) );
-        // Update the rbf values
-        for (int rgb=0;rgb<3;rgb++) {
-          rbf_vals(rgb) = rbf_vals(rgb) + (weights[pnt_idx][rgb] * dist(0));
-        }        
-      }
-      // RBF biases
-      for (int rgb=0;rgb<3;rgb++) {
-        rbf_vals(rgb) = rbf_vals(rgb) + (coefs[0][rgb]);
-        rbf_vals(rgb) = rbf_vals(rgb) + (coefs[1][rgb] * transformed(x,y,0));
-        rbf_vals(rgb) = rbf_vals(rgb) + (coefs[2][rgb] * transformed(x,y,1));
-        rbf_vals(rgb) = rbf_vals(rgb) + (coefs[3][rgb] * transformed(x,y,2));
-      }
-      // Write values to gammutmapped image 
-      for (int rgb=0;rgb<3;rgb++) {
-        transformed(x,y,rgb) = rbf_vals(rgb);
-      }
-
-
-    }
-  }
-*/
 
   return 0;
 }
