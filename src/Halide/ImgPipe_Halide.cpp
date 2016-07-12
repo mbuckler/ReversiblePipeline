0c17cfc9bd5034838ece82706b6c991348f3a2bd
mbuckler <markabuckler@gmail.com>
2016-07-12 19:51:26 -0400

    Halide pipeline with updated patch processing and separated schedule

--- src/Halide/ImgPipe_Halide.cpp
+++ src/Halide/ImgPipe_Halide.cpp
@@ -17,7 +17,7 @@ int run_pipeline(bool direction, bool full, int patchsize, int xstart, int ystar
 int main(int argc, char **argv) {
   using namespace std;
 
-  bool full = true;
+  bool full = false;
 
   int patchsize = 1;
   int xstart    = 551;
@@ -36,6 +36,9 @@ int main(int argc, char **argv) {
 
 // Reversible pipeline function
 int run_pipeline(bool direction, bool full, int patchsize, int xstart, int ystart) {
+
+  ///////////////////////////////////////////////////////////////////////////////////////
+  // Import and format model data
 
   using namespace std;  
 
@@ -64,7 +67,6 @@ int run_pipeline(bool direction, bool full, int patchsize, int xstart, int ystar
  
   // Take the transpose of the color map and white balance transform for later use
   vector<vector<float>> TsTw_tran = transpose_mat (TsTw);
-
 
   using namespace Halide;
   using namespace Halide::Tools;
@@ -97,107 +99,131 @@ int run_pipeline(bool direction, bool full, int patchsize, int xstart, int ystar
     }
   }
  
+  ///////////////////////////////////////////////////////////////////////////////////////
+  // Import and format input image
 
-  // Load input image 
-  Image<uint8_t> input = load_image(demosaiced_image);
   // Declare image handle variables
   Var x, y, c;
 
+  // Load input image 
+  Image<uint8_t> input = load_image(demosaiced_image);
+
+  // Initialize patch
+  Image<uint8_t> in_patch;
+
+  // Define simple copying function
+  Func pixel_copy("pixel_copy");
+  pixel_copy(x,y,c) = input(x,y,c);
+
+  // Resize the input if not processing full image 
+  if (full == true) {
+    // Define the patch as the full image
+    in_patch = input;
+  } else {
+    // Copy the subset of the image into the patch
+    Image<uint8_t> temp(patchsize,patchsize,3);
+    temp.set_min(xstart,ystart);
+    in_patch = temp;
+    pixel_copy.realize(in_patch);
+  }
+
+  ///////////////////////////////////////////////////////////////////////////////////////
+  // Halide Funcs for camera pipeline
 
   // Cast input to float and scale according to its 8 bit input format
-  Func input_scaled("input_scaled");
-  input_scaled(x,y,c)  = cast<float>(input(x,y,c))/256;
+  Func scale("scale");
+    scale(x,y,c) = cast<float>(in_patch(x,y,c))/256;
   
-
   // Color map and white balance transform
   Func transform("transform");
-  transform(x,y,c) = max( select(
-    // Perform matrix multiplication, set min of 0
-    c == 0, input_scaled(x,y,0)*TsTw_tran[0][0]
-          + input_scaled(x,y,1)*TsTw_tran[1][0]
-          + input_scaled(x,y,2)*TsTw_tran[2][0],
-    c == 1, input_scaled(x,y,0)*TsTw_tran[0][1]
-          + input_scaled(x,y,1)*TsTw_tran[1][1]
-          + input_scaled(x,y,2)*TsTw_tran[2][1],
-            input_scaled(x,y,0)*TsTw_tran[0][2]
-          + input_scaled(x,y,1)*TsTw_tran[1][2]
-          + input_scaled(x,y,2)*TsTw_tran[2][2])
-                          , 0);
-
-  //transform.trace_stores();
-
-  Image<float> transformed =
-    transform.realize(input.width(), input.height(), input.channels());
-
+    transform(x,y,c) = max( select(
+      // Perform matrix multiplication, set min of 0
+      c == 0, scale(x,y,0)*TsTw_tran[0][0]
+            + scale(x,y,1)*TsTw_tran[1][0]
+            + scale(x,y,2)*TsTw_tran[2][0],
+      c == 1, scale(x,y,0)*TsTw_tran[0][1]
+            + scale(x,y,1)*TsTw_tran[1][1]
+            + scale(x,y,2)*TsTw_tran[2][1],
+              scale(x,y,0)*TsTw_tran[0][2]
+            + scale(x,y,1)*TsTw_tran[1][2]
+            + scale(x,y,2)*TsTw_tran[2][2])
+                            , 0);
 
   // Weighted radial basis function for gamut mapping
   Func rbf_ctrl_pts("rbf_ctrl_pts");
-  // Initialization with all zero
-  rbf_ctrl_pts(x,y,c) = cast<float>(0);
-  // Index to iterate with
-  RDom idx(0,num_ctrl_pts);
-  // Loop code
-  // Subtract the vectors 
-  Expr red_sub   = transformed(x,y,0) - ctrl_pts_h(0,idx);
-  Expr green_sub = transformed(x,y,1) - ctrl_pts_h(1,idx);
-  Expr blue_sub  = transformed(x,y,2) - ctrl_pts_h(2,idx);
-  // Take the L2 norm to get the distance
-  Expr dist      = sqrt( red_sub*red_sub + green_sub*green_sub + blue_sub*blue_sub );
-  // Update persistant loop variables
-  rbf_ctrl_pts(x,y,c) = select( c == 0, rbf_ctrl_pts(x,y,c) + (weights_h(0,idx) * dist),
-                                c == 1, rbf_ctrl_pts(x,y,c) + (weights_h(1,idx) * dist),
-                                        rbf_ctrl_pts(x,y,c) + (weights_h(2,idx) * dist));
-  //rbf_ctrl_pts.trace_stores();
-
-  Image<float> after_ctrl_pts =
-    rbf_ctrl_pts.realize(input.width(), input.height(), input.channels());
- 
+    // Initialization with all zero
+    rbf_ctrl_pts(x,y,c) = cast<float>(0);
+    // Index to iterate with
+    RDom idx(0,num_ctrl_pts);
+    // Loop code
+    // Subtract the vectors 
+    Expr red_sub   = transform(x,y,0) - ctrl_pts_h(0,idx);
+    Expr green_sub = transform(x,y,1) - ctrl_pts_h(1,idx);
+    Expr blue_sub  = transform(x,y,2) - ctrl_pts_h(2,idx);
+    // Take the L2 norm to get the distance
+    Expr dist      = sqrt( red_sub*red_sub + green_sub*green_sub + blue_sub*blue_sub );
+    // Update persistant loop variables
+    rbf_ctrl_pts(x,y,c) = select( c == 0, rbf_ctrl_pts(x,y,c) + (weights_h(0,idx) * dist),
+                                  c == 1, rbf_ctrl_pts(x,y,c) + (weights_h(1,idx) * dist),
+                                          rbf_ctrl_pts(x,y,c) + (weights_h(2,idx) * dist));
 
   // Add on the biases for the RBF
   Func rbf_biases("rbf_biases");
-  rbf_biases(x,y,c) = max( select( 
-    c == 0, after_ctrl_pts(x,y,0) + coefs[0][0] + coefs[1][0]*transform(x,y,0) +
-      coefs[2][0]*transform(x,y,1) + coefs[3][0]*transform(x,y,2),
-    c == 1, after_ctrl_pts(x,y,1) + coefs[0][1] + coefs[1][1]*transform(x,y,0) +
-      coefs[2][1]*transform(x,y,1) + coefs[3][1]*transform(x,y,2),
-            after_ctrl_pts(x,y,2) + coefs[0][2] + coefs[1][2]*transform(x,y,0) +
-      coefs[2][2]*transform(x,y,1) + coefs[3][2]*transform(x,y,2))
-                          , 0);
-  rbf_biases.trace_stores();
-
+    rbf_biases(x,y,c) = max( select( 
+      c == 0, rbf_ctrl_pts(x,y,0) + coefs[0][0] + coefs[1][0]*transform(x,y,0) +
+        coefs[2][0]*transform(x,y,1) + coefs[3][0]*transform(x,y,2),
+      c == 1, rbf_ctrl_pts(x,y,1) + coefs[0][1] + coefs[1][1]*transform(x,y,0) +
+        coefs[2][1]*transform(x,y,1) + coefs[3][1]*transform(x,y,2),
+              rbf_ctrl_pts(x,y,2) + coefs[0][2] + coefs[1][2]*transform(x,y,0) +
+        coefs[2][2]*transform(x,y,1) + coefs[3][2]*transform(x,y,2))
+                            , 0);
 
   // Forward tone mapping
   Func tonemap("tonemap");
-  RDom idx2(0,256);
-  // Theres a lot in this one line! Functionality wise it finds the entry in 
-  // the reverse tone mapping function which is closest to the value found by
-  // gamut mapping. This entry is then scaled by 256 to ensure that the output
-  // is fractional, just like the rest of the pipeline.
-  tonemap(x,y,c) = argmin( abs( rev_tone_h(c,idx2) - rbf_biases(x,y,c) ) )[0]  / 256.0f;
-  //tonemap.trace_stores();
-
+    RDom idx2(0,256);
+    // Theres a lot in this one line! Functionality wise it finds the entry in 
+    // the reverse tone mapping function which is closest to the value found by
+    // gamut mapping. This entry is then scaled by 256 to ensure that the output
+    // is fractional, just like the rest of the pipeline.
+    tonemap(x,y,c) = argmin( abs( rev_tone_h(c,idx2) - rbf_biases(x,y,c) ) )[0]  / 256.0f;
 
   // Rescale the output and cast the output to 8 bit for image writing
   Func output_8("output_8");
-  output_8(x,y,c) = cast<uint8_t>(tonemap(x,y,c)*256);
-  //output_8.trace_stores();
+    output_8(x,y,c) = cast<uint8_t>(tonemap(x,y,c)*256);
+ 
+  output_8.trace_stores();
 
+  ////////////////////////////////////////////////////////////////////////
+  // CPU Schedule
 
-  // Realize the functions
+  transform.compute_root();
+
+  rbf_ctrl_pts.compute_root();
+
+  rbf_biases.compute_root();
+
+  Image<uint8_t> output;
+
+  // Resize the input if not processing full image 
   if (full == true) {
-    // Put full image through pipeline
-    Image<uint8_t> output =
-      output_8.realize(input.width(), input.height(), input.channels());
-    // Save the output for inspection
-    save_image(output, "output.png");
+    // patch as the full image
+    output = output_8.realize(in_patch.width(), 
+                              in_patch.height(), 
+                              in_patch.channels());
   } else {
-    // Only compute a patch
-    Image<uint8_t> output(patchsize,patchsize,3);
-    output.set_min(xstart,ystart);
+    // Copy the subset of the image into the patch
+    Image<uint8_t> temp(patchsize,patchsize,3);
+    temp.set_min(xstart,ystart);
+    output = temp;
     output_8.realize(output);
-    // Save the output for inspection
-    save_image(output, "output.png");
   }
+
+
+
+  // Save the output for inspection
+  save_image(output, "output.png");
+
+  ////////////////////////////////////////////////////////////////////////
 
   return 0;
 }
